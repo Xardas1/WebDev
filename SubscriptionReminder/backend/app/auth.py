93b24a96fb002d40e726ac23fd71366c63e6f898
Xardas1 <ciufel.maly@gmail.com>
2025-06-18 22:17:08 +0200

    olk

--- SubscriptionReminder/backend/app/auth.py
+++ SubscriptionReminder/backend/app/auth.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
-import jwt
 from fastapi import Depends, APIRouter, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jwt.exceptions import InvalidTokenError
