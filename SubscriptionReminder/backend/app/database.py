580c9e15c22949951e23d4297a7c2fc1470e50d9
Xardas1 <ciufel.maly@gmail.com>
2025-06-19 00:00:44 +0200

    olek

--- SubscriptionReminder/backend/app/database.py
+++ SubscriptionReminder/backend/app/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .models import Base
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:dupa123@localhost:5432/subscription_reminder"
+load_dotenv
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
